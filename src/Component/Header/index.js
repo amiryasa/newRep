d9bb5ac16248ff8bc67933efbc3c709b524df6d6
amirho31n <amirhossein.behbodi@gmail.com>
2022-07-28 11:50:03 +0430

    firstCommit

---
+++ src/Component/Header/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const Header = () => {
+  return (
+    <div class="header">
+      <div class="headerRow headerLogo"> آکادمی کدنویسی بحر </div>
+      <div class="headerRow headerNavbar"> hi hi </div>
+      <div class="headerRow headerLogin"> hi hi hi </div>
+    </div>
+  );
+};
+
+export { Header };
